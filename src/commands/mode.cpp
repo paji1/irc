79c874c01685fcd9581d8b75706dea28bc1e3ab3
Paji1 <tahamaroc1@gmail.com>
2023-07-27 09:13:53 +0100

    chore edit format user and join

--- src/commands/mode.cpp
+++ src/commands/mode.cpp
@@ -1,8 +1,5 @@
 #include "Commands.hpp"
 
-
-
-
 static std::string extract_token(std::stringstream &src)
 {
 	std::string temp;
@@ -19,26 +16,26 @@ static std::string extract_options(std::string &_src)
 	std::string temp;
 
 	if (_src.empty())
-		return std::string(); // maybe do in upper layer 
-	
+		return std::string(); // maybe do in upper layer
+
 	std::string src = _src;
 	src = *(src.begin() + 1);
 	size_t pos = src.find_first_of("+-");
-	temp = _src.substr(0 , pos);
+	temp = _src.substr(0, pos);
 	if (pos == src.npos)
-		return  _src = "", temp;
+		return _src = "", temp;
 	_src.erase(_src.begin(), _src.begin() + pos);
 	return temp;
 }
 
-static char extract_option(std::string &src, char& sign)
+static char extract_option(std::string &src, char &sign)
 {
 	char temp;
 	int offset = 1;
 
 	temp = src[1];
 	src[1] = src[0];
-	sign =  src[0];
+	sign = src[0];
 	if (src.size() >= 3 && (src[2] == '+' || src[2] == '-'))
 		offset++;
 	src.erase(src.begin(), src.begin() + offset);
@@ -51,10 +48,10 @@ static std::string extract_argument(std::string &src)
 	std::stringstream stream;
 
 	if (src.empty())
-		return std::string(); // maybe do in upper layer 
-	stream  << src;
+		return std::string(); // maybe do in upper layer
+	stream << src;
 	stream >> temp;
-	src.clear(); 
+	src.clear();
 	getline(stream, src);
 	return temp;
 }
@@ -81,8 +78,7 @@ static std::string extract_token_args(std::stringstream &src, std::string &mod)
 	return args;
 }
 
-
-Channel& Commands::check_channel(std::string &channel)
+Channel &Commands::check_channel(std::string &channel)
 {
 	if (!channel.size())
 		throw std::string("ERR_NEEDMOREPARAMS");
@@ -100,13 +96,13 @@ void check_modestring(std::string &op)
 {
 	size_t size = op.size();
 	if (!size)
-		return ;
+		return;
 	if (op.at(0) != '-' && op.at(0) != '+')
 		throw std::string("RPL_CHANNELMODEIS");
 	if ((op.at(0) != '+' && op.at(0) != '-') || op.size() < 2)
 		throw std::string("RPL_CHANNELMODEIS");
 	std::string ops = "oiktl+-";
-	for (int i = 1; op[i] ; i++)
+	for (int i = 1; op[i]; i++)
 	{
 		if ((op.at(i) == '-' || op.at(i) == '+') && (i == (int)size - 1 || !isalpha(op.at(i + 1))))
 			throw std::string("RPL_CHANNELMODEIS");
@@ -115,11 +111,9 @@ void check_modestring(std::string &op)
 	}
 }
 
-
 optr get_option(char op)
 {
 	std::map<char, optr> commands;
-
 
 	commands['i'] = &Commands::mode_i;
 	commands['t'] = &Commands::mode__t;
@@ -131,9 +125,9 @@ optr get_option(char op)
 	{
 		return commands.at(op);
 	}
-	catch(const std::exception& e)
+	catch (const std::exception &e)
 	{
-    	return NULL;
+		return NULL;
 	}
 }
 
@@ -141,15 +135,14 @@ void get_params(std::pair<std::string, std::string> &map_param, std::stringstrea
 {
 	std::string mod;
 	std::string modestring;
-	
+
 	modestring = extract_token(stream);
 	std::string mode_arguments = extract_token_args(stream, mod);
 	check_modestring(modestring);
 	map_param = std::make_pair(modestring, mode_arguments);
 }
 
-
-void  Commands::get_option_and_args(Channel& channel, User &user, std::pair<std::string, std::string> & map_param)
+void Commands::get_option_and_args(Channel &channel, User &user, std::pair<std::string, std::string> &map_param)
 {
 	char sign;
 	std::string args_left;
@@ -164,7 +157,7 @@ void  Commands::get_option_and_args(Channel& channel, User &user, std::pair<std:
 
 			if (modeptr != NULL)
 			{
-				args_left = (this->*modeptr)(channel, user, extract_argument(map_param.second),sign);
+				args_left = (this->*modeptr)(channel, user, extract_argument(map_param.second), sign);
 				if (!args_left.empty())
 				{
 					map_param.second = args_left + " " + map_param.second;
@@ -174,8 +167,7 @@ void  Commands::get_option_and_args(Channel& channel, User &user, std::pair<std:
 	}
 }
 
-
-void	Commands::mode(Client *client, std::stringstream &stream)
+void Commands::mode(Client *client, std::stringstream &stream)
 {
 	Message message(*client, "MODE", client->_client_user.nickname);
 	std::pair<std::string, std::string> pair_param;
@@ -183,45 +175,44 @@ void	Commands::mode(Client *client, std::stringstream &stream)
 	std::string time;
 
 	stream >> channel;
-	
+
 	try
 	{
-		Channel& ch = check_channel(channel);
+		Channel &ch = check_channel(channel);
 		get_params(pair_param, stream);
 		if (pair_param.first.empty())
 		{
 			time = ch._create_time;
-			throw  std::string("RPL_CREATIONTIME");
+			throw std::string("RPL_CREATIONTIME");
 		}
 		msg_tmp = &message;
-		get_option_and_args(ch , client->_client_user , pair_param);
-
+		get_option_and_args(ch, client->_client_user, pair_param);
 	}
-	catch(std::string& e)
+	catch (std::string &e)
 	{
 		if (e == "ERR_UMODEUNKNOWNFLAG")
 		{
-			message.set_message_error(ERR_UMODEUNKNOWNFLAG(_server->serverName,client->_client_user.nickname));
+			message.set_message_error(ERR_UMODEUNKNOWNFLAG(_server->serverName, client->_client_user.nickname));
 			_server->sendMessage_err(message);
 		}
 		else if (e == "ERR_NOSUCHCHANNEL")
 		{
-			message.set_message_error(ERR_NOSUCHCHANNEL(_server->serverName, client->_client_user.nickname ,channel));
+			message.set_message_error(ERR_NOSUCHCHANNEL(_server->serverName, client->_client_user.nickname, channel));
 			_server->sendMessage_err(message);
 		}
 		else if (e == "ERR_NEEDMOREPARAMS")
 		{
-			message.set_message_error(ERR_NEEDMOREPARAMS(_server->serverName, client->_client_user.nickname ,"MODE"));
+			message.set_message_error(ERR_NEEDMOREPARAMS(_server->serverName, client->_client_user.nickname, "MODE"));
 			_server->sendMessage_err(message);
 		}
 		else if (e == "ERR_CHANOPRIVSNEEDED")
 		{
-			message.set_message_error(ERR_CHANOPRIVSNEEDED(_server->serverName, client->_client_user.nickname ,channel));
+			message.set_message_error(ERR_CHANOPRIVSNEEDED(_server->serverName, client->_client_user.nickname, channel));
 			_server->sendMessage_err(message);
 		}
 		else if (e == "RPL_CREATIONTIME")
 		{
-			message.set_message_error(RPL_CREATIONTIME(_server->serverName, client->_client_user.nickname ,channel, time));
+			message.set_message_error(RPL_CREATIONTIME(_server->serverName, client->_client_user.nickname, channel, time));
 			_server->sendMessage_err(message);
 		}
 	}
