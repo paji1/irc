b6905761c5f277ac52884fa3f43daa4e4c69b0fb
Paji1 <tahamaroc1@gmail.com>
2023-07-27 09:12:43 +0100

    chore edit format priv and quit

--- src/commands/quit.cpp
+++ src/commands/quit.cpp
@@ -6,24 +6,24 @@
 /*   By: tel-mouh <tel-mouh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 21:13:46 by ozahir            #+#    #+#             */
-/*   Updated: 2023/07/27 03:19:50 by tel-mouh         ###   ########.fr       */
+/*   Updated: 2023/07/27 09:12:18 by tel-mouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Commands.hpp"
 #include "Server.hpp"
 
-void	Commands::quit(Client *client, std::stringstream &stream)
+void Commands::quit(Client *client, std::stringstream &stream)
 {
     std::string reason;
-    std::string buffer; 
+    std::string buffer;
     while (!stream.eof())
     {
         stream >> buffer;
         reason += buffer + " ";
     }
     if (reason.length() && reason[0] == ':')
-        reason.erase(0,1);
+        reason.erase(0, 1);
     Message message(*client, "QUIT", client->_client_user.nickname);
     if (reason.length())
         message.set_trailing(reason);
