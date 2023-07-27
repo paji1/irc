97be2c83ec5ead0d709177d9c1e20b25bf6bac1e
Paji1 <tahamaroc1@gmail.com>
2023-07-27 09:18:20 +0100

    chore edit format bot cap commands

--- src/commands/invite.cpp
+++ src/commands/invite.cpp
@@ -1,9 +1,6 @@
-# include <Commands.hpp>
+#include <Commands.hpp>
 
-
-
-
-void Commands::	invite(Client *client, std::stringstream &_stream)
+void Commands::invite(Client *client, std::stringstream &_stream)
 {
 	Message message(*client, "INVITE", client->_client_user.nickname);
 	std::string nickname;
@@ -23,29 +20,29 @@ void Commands::	invite(Client *client, std::stringstream &_stream)
 		if (ch.get_user(nickname))
 			throw std::string("ERR_USERONCHANNEL");
 		ch.inveted.insert(nickname);
-		message.set_message_error(RPL_INVITING(_server->serverName, client->_client_user.nickname ,channel, nickname));
+		message.set_message_error(RPL_INVITING(_server->serverName, client->_client_user.nickname, channel, nickname));
 		_server->sendMessage_err(message);
 	}
-	catch(std::string e)
+	catch (std::string e)
 	{
 		if (e == "ERR_NOSUCHCHANNEL")
 		{
-			message.set_message_error(ERR_NOSUCHCHANNEL(_server->serverName, client->_client_user.nickname ,channel));
+			message.set_message_error(ERR_NOSUCHCHANNEL(_server->serverName, client->_client_user.nickname, channel));
 			_server->sendMessage_err(message);
 		}
 		else if (e == "ERR_NEEDMOREPARAMS")
 		{
-			message.set_message_error(ERR_NEEDMOREPARAMS(_server->serverName, client->_client_user.nickname ,"INVITE"));
+			message.set_message_error(ERR_NEEDMOREPARAMS(_server->serverName, client->_client_user.nickname, "INVITE"));
 			_server->sendMessage_err(message);
 		}
 		else if (e == "ERR_CHANOPRIVSNEEDED")
 		{
-			message.set_message_error(ERR_CHANOPRIVSNEEDED(_server->serverName, client->_client_user.nickname ,channel));
+			message.set_message_error(ERR_CHANOPRIVSNEEDED(_server->serverName, client->_client_user.nickname, channel));
 			_server->sendMessage_err(message);
 		}
 		else if (e == "ERR_USERONCHANNEL")
 		{
-			message.set_message_error(ERR_USERONCHANNEL(_server->serverName, client->_client_user.nickname ,nickname,channel));
+			message.set_message_error(ERR_USERONCHANNEL(_server->serverName, client->_client_user.nickname, nickname, channel));
 			_server->sendMessage_err(message);
 		}
 	}
